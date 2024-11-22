536bf27abb374c693e28f50340dc7185e555d1f8
Austin3535 <auem6516@colorado.edu>
2024-11-21 20:50:33 -0700

    Initial Commit

---
+++ Git?/Git_App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_App.swift
+//  Git?
+//
+//  Created by Austin Emfield on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Git?Tests/Git_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Tests.swift
+//  Git?Tests
+//
+//  Created by Austin Emfield on 11/21/24.
+//
+
+import Testing
+@testable import Git_
+
+struct Git_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
